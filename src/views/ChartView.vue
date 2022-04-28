c110bd24131fb7aa9093704f04ead2b0f5d17b9c
unknown <iamjimmyjimmy@yahoo.com>
2022-04-27 21:59:37 -0700

    Edit: Fixed Canvas Overflow on Mobile

--- src/views/ChartView.vue
+++ src/views/ChartView.vue
@@ -2,7 +2,7 @@
   <div class="chart">
     <h1 class="title is-1 text-left">Resource Charts</h1>
     <div class="grid">
-      <div class="col">
+      <div class="col-12 lg:col-6">
         <Card>
           <template #title>
             <h3 class="title is-3">Resource Per Stage</h3>
@@ -12,7 +12,7 @@
           </template>
         </Card>
       </div>
-      <div class="col">Other Charts</div>
+      <div class="col-12 lg:col-6">Other Charts</div>
     </div>
   </div>
 </template>
